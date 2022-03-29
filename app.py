5da6a279e420130ec1c6489eca30104105bd72ec
Albert Merino <albert.merino@eurecat.org>
2022-03-29 14:06:11 +0200

    Init

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home_view():
+    return "<h1>Welcome to Geeks for Geeks</h1>"
+
+
+if __name__ == "__main__":
+    app.run()
